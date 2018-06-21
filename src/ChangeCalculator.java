e7b6e7850953d5b053125b1b12372e5fe03bd9b1
League Student <league@adminisorsiMac3.attlocal.net>
2018-06-20 19:53:53 -0700

    ChangeCaluclator
    My last class before a 6 weeks break.

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -9,12 +9,26 @@ import javax.swing.JOptionPane;
  * will calculate how much money I have without me having to use my brain. Then
  * make me a sandwich.
  */
+
 public class ChangeCalculator {
 
 	public static void main(String[] args) {
 
-		// Ask the user how many nickels they have
-
+		String ans1 = JOptionPane.showInputDialog("How many nickels do you have?");
+		
+		String ans2 = JOptionPane.showInputDialog("How many dimes do you have?");
+		
+		String ans3 = JOptionPane.showInputDialog("How many quarters do you have");
+		
+		int a = Integer.parseInt(ans1);
+		int b = Integer.parseInt(ans2);
+		int c = Integer.parseInt(ans3);
+		
+		double d = a * 0.05;
+		double e = b * 0.1;
+		double f = c * 0.25;
+		
+		double n = d + e + f;
 		// Convert their answer to an int using Integer.parseInt()
 
 		// Ask the user how many dimes they have, and convert their answer
@@ -24,6 +38,7 @@ public class ChangeCalculator {
 		// Calculate how much money the user has and save it in a double variable 
 
 		// Tell the user how much money they have
+		JOptionPane.showMessageDialog(null, "You have $" + n);
 
 	}
 }
